01bcf217591d887d33147137a66e263a35d889be
Kullasup Phongsrisuk <phongsrisuk@gmail.com>
2024-05-16 16:54:09 +0700

    บันทึก

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:example3/screen/my_home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,10 @@ class MyApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
   }
 }
       
